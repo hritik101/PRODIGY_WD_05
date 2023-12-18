a1587bca1ebbf3ec77674fff6d3c6191a5e08276
hritik101 <89718959+hritik101@users.noreply.github.com>
2023-12-18 20:08:26 +0530

    Add files via upload

---
+++ utils/weatherData.js
@@ -0,0 +1,28 @@
+const request = require("request");
+
+const openWeatherMap = {
+  BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+  SECRET_KEY: "fdb0a72afba5a8b5c2fd9c0eadf38ea9",
+};
+
+const weatherData = (address, callback) => {
+  const url =
+    openWeatherMap.BASE_URL +
+    encodeURIComponent(address) +
+    "&APPID=" +
+    openWeatherMap.SECRET_KEY;
+  console.log(url);
+  request({ url, json: true }, (error, data) => {
+    if (error) {
+      callback(true, "Unable to fetch data, Please try again. " + error);
+    }
+    callback(false, data?.body);
+  });
+};
+
+module.exports = weatherData;
+
+a = 12;
+b = 25;
+
+console.log(a + b);
